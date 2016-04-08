4ab0054e070605dd53c0fab2cc258a76bb8eeecc
Ena Bek <enabek@gmail.com>
2016-04-08 17:15:34 -0400

    Add legit stuff

---
+++ extra-sticky.js
@@ -0,0 +1,8 @@
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    if (scroll >= 400) {
+        $("#sticker").addClass("toColor");
+    } else {
+    	$("#sticker").removeClass("toColor")
+    }
+});
