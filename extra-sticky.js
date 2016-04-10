1975c5f73adb03c28ee080430828f969c7598fe4
Ena Bek <enabek@gmail.com>
2016-04-10 14:56:52 -0400

    Add complete changes

--- extra-sticky.js
+++ extra-sticky.js
@@ -1,13 +1,12 @@
-$(window).scroll(function() {    
+$(window).scroll(function() {
     var scroll = $(window).scrollTop();
-    if (scroll >= 400) {
-        $("#sticker").addClass("toColor");
+    if (scroll <= 400) {
+        $("#sticker").addClass("backColor");
+        $('.name').removeClass('trans');
+        $('#dot').removeClass('dot-color');
     } else {
-    	$("#sticker").removeClass("toColor")
+    	$("#sticker").removeClass("backColor").addClass("toColor");
+      $('.name').addClass('trans');
+      $('#dot').addClass('dot-color');
     }
 });
-
-
-// $('#sticker').onePageNav({
-// 	filter: ':not(.external)'
-// });
