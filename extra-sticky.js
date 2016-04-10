9a4c8d4b27bceab3ba0c14f6e2d925b979e87d02
Ena Bek <enabek@gmail.com>
2016-04-10 15:38:29 -0400

    Add final final changes

--- extra-sticky.js
+++ extra-sticky.js
@@ -4,9 +4,36 @@ $(window).scroll(function() {
         $("#sticker").addClass("backColor");
         $('.name').removeClass('trans');
         $('#dot').removeClass('dot-color');
-    } else {
-    	$("#sticker").removeClass("backColor").addClass("toColor");
-      $('.name').addClass('trans');
-      $('#dot').addClass('dot-color');
+
+        var links = $(".nav-link");
+        if ( links.hasClass("active") ) {
+          links.removeClass("active");
+        }
+        $('.home').addClass("active");
+
+    } else if ( scroll >= 500 && scroll <= 1200) {
+        $("#sticker").removeClass("backColor").addClass("toColor");
+        $('.name').addClass('trans');
+        $('#dot').addClass('dot-color');
+
+        var links = $(".nav-link");
+        if ( links.hasClass("active") ) {
+          links.removeClass("active");
+        }
+        $('.about').addClass("active");
+
+    } else if ( scroll >= 1300 && scroll <= 1500 ) {
+        var links = $(".nav-link");
+        if ( links.hasClass("active") ) {
+          links.removeClass("active");
+        }
+        $('.projects').addClass("active");
+
+    } else if ( scroll >= 1800 ) {
+        var links = $(".nav-link");
+        if ( links.hasClass("active") ) {
+          links.removeClass("active");
+        }
+        $('.contact').addClass("active");
     }
 });
