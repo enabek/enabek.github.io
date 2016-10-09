b11669e3086c236ec7372ff77d64e2f5459c748b
Ena Bek <enabek@gmail.com>
2016-10-09 12:51:44 -0400

    Change speed of animation and clean  up code

--- effects.js
+++ effects.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
   $(window).scroll(function() {
-    
+
     $(".hidden").each(function(elm) {
       var bottom_of_object = $(this).offset().top + $(this).outerHeight();
       var bottom_of_window = $(window).scrollTop() + $(window).height();
 
       if( bottom_of_window > bottom_of_object ){
-        $(this).animate({'opacity':'1'},700);
+        $(this).animate({'opacity':'1'},1000);
       };
-    }) 
+    });
+    
   });
 });
